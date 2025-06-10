4fe04271d57f8ee58ba52bbcb442e03a87483705
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2025-06-10 13:30:53 +0300

    login fixed with hardcoded user id

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 // Allow CORS
 header("Access-Control-Allow-Origin: http://10.4.96.116");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
